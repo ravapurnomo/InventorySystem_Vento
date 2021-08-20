5b5399a48aba9df660e7ce94f8702dfdfd3586cb
ravapurnomo <mochravanel@gmail.com>
2021-08-20 16:35:28 +0700

    XML
    UpdateAct :
    Tambah EditText Keterangan
    Button Perbarui jadi Button Masuk
    Bikin Button Keluar
    Bikin Lihat Detail Barang
    XMLbutton untuk Masuk,Keluar, Hapus
    
    AddActivity :
    Tambah EditText Keterangan
    
    Tambah warna baru : Abu, Hijau
    
    Java :
    Buat class baru SeeDetail_Item.java untuk button LiharDetailBarang di ActivityUpdate.java

---
+++ app/src/main/java/com/rava/ventoapp/version2/SeeDetail_Item.java
@@ -0,0 +1,9 @@
+package com.rava.ventoapp.version2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SeeDetail_Item extends AppCompatActivity {
+
+    RecyclerView recyclerView_SeeDetailItem;
+}
